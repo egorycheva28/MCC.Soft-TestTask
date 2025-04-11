438087c342f0611abe9e0f68021385de183547de
egorycheva28 <egor.masha28@list.ru>
2025-04-11 13:35:59 +0700

    добавление, удаление и вывод отдельных элементов

--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -2,15 +2,23 @@ import React, { useState, useEffect } from "react";
 import "./mainComponent.css";
 import Modal from "react-modal";
 import Element from "./componentItem";
+import store from "../store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { addElementThunkCreator, getTreeThunkCreator, removeElementThunkCreator } from "../reducers/treeReducer";
 
 Modal.setAppElement('#root');
 
-const Tree = () => {
-    const [tree, setTree] = useState("");
+const Tree = ({ treeReducers }) => {
+    const dispatch = useDispatch()
+
+    const [selectedId, setSelectedId] = useState(null);
+    const [tree, setTree] = useState();
     const [addModalIsOpen, setAddModalIsOpen] = useState(false);
     const [editModalIsOpen, setEditModalIsOpen] = useState(false);
     const [name, setName] = useState("");
-    const [currentElement, setCurrentElement] = useState();
+    const [currentElement, setCurrentElement] = useState(1);
+    const [parent, setParent] = useState("");
+    const trees = useSelector(state => state.treeReducers.treess);
 
     const openAddModal = () => {
         setAddModalIsOpen(true);
@@ -28,7 +36,14 @@ const Tree = () => {
         setEditModalIsOpen(false);
     };
 
-    const addElement = () => {
+    const addElement = async () => {
+        const newElement = {
+            id: currentElement,
+            name: name
+        };
+        await dispatch(addElementThunkCreator(newElement));
+        setCurrentElement(currentElement+1);
+        //setTree();
         setTree(name);
     };
 
@@ -36,18 +51,47 @@ const Tree = () => {
 
     };
 
-    const removeElement = () => {
-
+    const removeElement = async () => {
+        dispatch(removeElementThunkCreator(selectedId));
+        const da = await dispatch(getTreeThunkCreator());
+        setTree(da);
     };
 
     const resetTree = () => {
-        setTree("");
+        setTree();
     };
 
     useEffect(() => {
-        setTree("Hello");
-    }, []);
+       
+        const fetchData = async () => {
 
+            //setTree("Hello");
+            /*let data = {
+                id: 1,
+                name: "qwe"
+            };
+            await dispatch(addElementThunkCreator(data));*/
+           
+
+            dispatch(getTreeThunkCreator());
+            setTree(store.getState());
+            //console.log(da);
+            console.log(store.getState().treeReducers.treess);
+            //console.log(store.getState().treeReducer.element);
+            //console.log(trees);
+
+        };
+        /*{
+                                    treeReducer.element.map((value) => {
+                                        <Element name={value.name} id={value.id} key={value.id} />
+                                    })
+                                } */
+        fetchData();
+    }, [dispatch]);
+    console.log(trees);
+    console.log(selectedId);
+    console.log(store.getState());
+    //console.log(treeReducer.treess);
     return (
         <div className="container">
             <div className="box">
@@ -56,8 +100,12 @@ const Tree = () => {
                 </div>
                 <div className="row2">
                     <div className="elements">
-                        <Element />
-                        <Element />
+                        {
+                            trees.map((value) => (
+                                <Element name={value.name} id={value.id} key={value.id} setSelectedId={setSelectedId}/>
+                            ))
+                        }
+
                     </div>
                 </div>
                 <div className="row3">
@@ -69,7 +117,7 @@ const Tree = () => {
                             <label>Название</label>
                             <input type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='Введите название элемента' ></input>
                             <label>родительский элемент</label>
-                            <input></input>
+                            <input type='text' value={parent} onChange={(e) => setParent(e.target.value)}></input>
                             <button onClick={addElement}>Add</button>
                         </div>
                     </Modal>
