e5ef2960b43e17789d51de9bc28420c7f7f1cbe2
egorycheva28 <egor.masha28@list.ru>
2025-04-11 13:37:55 +0700

    добавлено выделение элемента при клике на него и запоминание его id

--- src/components/componentItem.js
+++ src/components/componentItem.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./mainComponent.css";
 
-const Element = () => {
+const Element = (props) => {
+    const [color, setColor] = useState("selectedElement");
+
+    const selectedElement = (e) => {
+        //props.setSelectedId(null);
+        console.log("asd");
+        e.stopPropagation();
+        props.setSelectedId(props.id);
+        setColor("unselectedElement");
+    }
+
+    const unselectedElement = () => {
+        setColor("selectedElement");
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (event.target.closest('.row3') === null) {
+                unselectedElement();
+                props.setSelectedId(null);
+            }
+        };
+        console.log("asasasasas");
+
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+
+    }, [props]);
+
     return (
-        <div className="element">
-            <span className="name">name</span>
+        <div className={color}>
+            <span className="name" onClick={selectedElement}>{props.name}</span>
         </div>
     );
 };
